20f66d25020078a92b4089902257f2afe8b1dada
Thomas Le <97923129+tommykhoale@users.noreply.github.com>
2022-05-28 21:25:12 -0400

    Add files via upload

---
+++ New folder/HomePageAPICall/home.js
@@ -0,0 +1,5 @@
+fetch("https://www.boredapi.com/api/activity/?type=recreational")
+    .then(res => res.json())
+    .then(data=> {
+        document.getElementById('challenge').innerHTML = `${data.activity}`
+    })
